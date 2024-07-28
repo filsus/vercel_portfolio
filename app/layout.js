f31c85579099e04f946ff5016f5a338f1ac8bb70
Filip Suska <suska.filip@gmail.com>
2024-07-28 16:30:22 +0100

    changes to metadata

--- app/layout.js
+++ app/layout.js
@@ -6,11 +6,32 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Filip Suska - VFX",
   description: "Welcome to my portfolio website. Learn more about the stuff I do!",
+  image: "/images/main.png", // Replace with the actual path to your image
+  url: "https://www.filipsuska.com", // Replace with your actual website URL
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="og:url" content={metadata.url} />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={metadata.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="msapplication-TileImage" content={metadata.image} />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta itemProp="name" content={metadata.title} />
+        <meta itemProp="description" content={metadata.description} />
+        <meta itemProp="image" content={metadata.image} />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
